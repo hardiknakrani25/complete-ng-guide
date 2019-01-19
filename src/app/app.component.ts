4b76cd960f05b69d9a54d26d63aacd977ab5c134
Hardik Nakrani <hdnakarani@gmail.com>
2019-01-19 16:04:32 +0530

    Building and Using a Dropdown Directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-loadedfeature = '';
+loadedfeature = 'recipe';
   onNavigate(feature: string) {
     this.loadedfeature = feature;
   }
