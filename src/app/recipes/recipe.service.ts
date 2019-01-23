40828eb91694a116e9c106474efd0f1d911b3430
Hardik Nakrani <hdnakarani@gmail.com>
2019-01-23 14:01:04 +0530

    service for cross component communications

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,5 +1,8 @@
 import { Recipe } from './recipe.model';
+import {EventEmitter} from '@angular/core';
 export class RecipeService {
+  recipeSelected = new EventEmitter<Recipe>();
+
   private recipes: Recipe[] = [
     new Recipe('Test recipe1', 'test1', 'https://www.sbs.com.au/food/sites/sbs.com.au.food/files/IMG_1105.jpg'),
     new Recipe('Test recipe2', 'test2', 'https://www.sbs.com.au/food/sites/sbs.com.au.food/files/IMG_1105.jpg'),
